ff67c6ba2d7d43d8a9213863315f428e6b2ba7a9
daniheras <danielherascabrera@gmail.com>
2024-11-25 12:09:33 +0100

    feat: :sparkles: implement dynamic DarkModeButton with animation and lazy loading in ProfileMenu

--- src/app/(crm)/_components/DarkModeButton.tsx
+++ src/app/(crm)/_components/DarkModeButton.tsx
@@ -3,13 +3,39 @@
 import { useTheme } from "@/app/_shared/hooks/useTheme";
 import { ProfileMenuButton } from "./ProfileMenuButton";
 import { SunIcon } from "@/app/_shared/icons/Sun";
+import { MoonIcon } from "@/app/_shared/icons/Moon";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const DarkModeButton = () => {
-  const { toggleTheme } = useTheme();
+  const { toggleTheme, isDark } = useTheme();
 
   return (
-    <ProfileMenuButton ariaLabel="Toggle dark mode" onClick={toggleTheme} className="hidden md:flex">
-      <SunIcon />
-    </ProfileMenuButton>
+      <ProfileMenuButton 
+        ariaLabel="Toggle dark mode"
+        onClick={toggleTheme}
+        className="hidden md:flex"
+      >
+        <AnimatePresence mode="wait">
+          {isDark ? (
+            <motion.div
+              key="dark"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <SunIcon />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="light"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <MoonIcon />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </ProfileMenuButton>
   );
 };
--- src/app/(crm)/_components/ProfileMenu.tsx
+++ src/app/(crm)/_components/ProfileMenu.tsx
@@ -1,13 +1,20 @@
 
+"use client"
+
 import { ProfileMenuButton } from "./ProfileMenuButton";
 import { MenuIcon } from "@/app/_shared/icons/Menu";
-import { DarkModeButton } from "./DarkModeButton";
 import { Avatar } from "@/app/_shared/components/Avatar/Avatar";
+import dynamic from "next/dynamic";
+
+const SetThemeButton = dynamic(() => import("./DarkModeButton").then(mod => mod.DarkModeButton), {
+  ssr: false,
+  loading: () => <ProfileMenuButton/>,
+})
 
 export const ProfileMenu = () => {
   return (
     <div className="flex gap-3 flex-grow md:flex-grow-0 justify-end z-10">
-      <DarkModeButton/>
+      <SetThemeButton/>
       <ProfileMenuButton ariaLabel="Main menu" className="md:hidden">
         <MenuIcon />
       </ProfileMenuButton>
