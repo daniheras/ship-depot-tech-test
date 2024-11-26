d21a1c1bd895d951bbc6d6f261f357ade77d7840
daniheras <danielherascabrera@gmail.com>
2024-11-26 02:05:15 +0100

    feat: :fire: remove Card component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/app/_shared/components/Button/Button";
-import { Card } from "@/app/_shared/components/Card/Card";
 import { Input } from "@/app/_shared/components/Input";
 import { signIn } from "@/app/(auth)";
 import { StickyNote } from "./components/StickyNote";
@@ -10,30 +9,34 @@ export default function LoginPage() {
       action={async (formData) => {
         "use server";
         const plainFormData = Object.fromEntries(formData.entries());
-        await signIn("credentials", {...plainFormData, redirectTo: "/"});
+        await signIn("credentials", { ...plainFormData, redirectTo: "/" });
       }}
     >
       <StickyNote />
-      <Card>
-        <div className="flex flex-col gap-4 p-6">
-          <Input
-            label="Email"
-            name="email"
-            type="email"
-            placeholder="Your email"
-          ></Input>
-          <Input
-            label="Password"
-            type="password"
-            placeholder="Your password"
-            name="password"
-          />
-          <Input label="Remember me" type="checkbox" name="remember" />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="relative mx-auto w-full max-w-[24rem] rounded-xl overflow-hidden shadow-sm dark:border dark:border-taupe-200">
+          <div className="relative flex flex-col bg-white dark:bg-taupe-300">
+            <div className="flex flex-col gap-4 p-6">
+              <Input
+                label="Email"
+                name="email"
+                type="email"
+                placeholder="Your email"
+              ></Input>
+              <Input
+                label="Password"
+                type="password"
+                placeholder="Your password"
+                name="password"
+              />
+              <Input label="Remember me" type="checkbox" name="remember" />
+            </div>
+            <div className="p-6 pt-0">
+              <Button type="submit">Sign In</Button>
+            </div>
+          </div>
         </div>
-        <div className="p-6 pt-0">
-          <Button type="submit">Sign In</Button>
-        </div>
-      </Card>
+      </div>
     </form>
   );
 }
