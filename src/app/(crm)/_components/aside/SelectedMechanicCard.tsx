62f03591d56ee2e361a48c4229a9bdf959aa9716
daniheras <danielherascabrera@gmail.com>
2024-11-26 19:28:07 +0100

    style: replace planet image url with self hosted one

--- src/app/(crm)/_components/aside/SelectedMechanicCard.tsx
+++ src/app/(crm)/_components/aside/SelectedMechanicCard.tsx
@@ -11,7 +11,7 @@ export const SelectedMechanicCard = () => {
   );
 
   return (
-    <Card className="flex items-center gap-2 bg-[url('https://images.unsplash.com/photo-1590732488817-d34f0d6237a2?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-contain bg-blend-exclusion bg-no-repeat bg-right">
+    <Card className="flex items-center gap-2 dark:bg-[url('/planet.webp')] bg-contain bg-blend-exclusion bg-no-repeat bg-right">
       <Avatar className="w-8 h-8 cursor-default" img={mechanic?.image} />
       <div className="flex flex-col justify-center">
         <span className="text-sm dark:text-gray-200 text-gray-800 font-medium">
