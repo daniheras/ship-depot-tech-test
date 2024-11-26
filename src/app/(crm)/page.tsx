eade6b9b2c0484b4752c2057325aac206ebc90fe
daniheras <danielherascabrera@gmail.com>
2024-11-26 10:47:26 +0100

    feat: :chart_with_upwards_trend: add charts support

--- src/app/(crm)/page.tsx
+++ src/app/(crm)/page.tsx
@@ -1,3 +1,4 @@
+import { ActiveCases } from "./_components/charts/ActiveCases";
 import { CasesWrapper } from "./cases/components/CasesWrapper";
 
 export default function Dashboard() {
@@ -5,7 +6,7 @@ export default function Dashboard() {
     <>
       <div className="h-full grid grid-cols-1 lg:grid-cols-2 grid-rows-3 lg:grid-rows-2 gap-4">
         <div className="col-start-1 col-end-2 row-start-1 row-end-2 ">
-          Top Left
+          <ActiveCases/>
         </div>
         <div className="col-start-1 col-end-2 row-start-3 row-end-4 lg:row-start-2 lg:row-end-3 ">
           Bottom Left
